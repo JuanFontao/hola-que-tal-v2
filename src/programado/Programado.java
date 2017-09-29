a7c82e5363cf9b3971f57fceef3dc9638f96bbb4
jperezfontao <jperezfontao@victoria25.danielcastelao.org>
2017-09-29 09:25:25 +0200

    Segundo Hola Mundo

---
+++ src/programado/Programado.java
@@ -0,0 +1,15 @@
+/*
+*/
+package programado;
+
+
+public class Programado {
+
+   
+   public static void main(String[] args) {
+        // programa de saudo
+        System.out.println("Hola Mundo");
+               
+    }
+    
+}
